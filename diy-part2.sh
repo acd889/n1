4586876c3694f6fa34582fcc713ff687658f23ac
liu ta <34493169+acd889@users.noreply.github.com>
2024-10-25 06:33:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,7 +26,7 @@ git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/l
 # 加入E8820S支持
 #wget https://gitee.com/jin3014/openwrt-swind/raw/master/target/linux/ramips/dts/mt7621_zte_e8820s.dts
 #rsync -a  mt7621_* target/linux/ramips/dts
-wget https://gitee.com/jin3014/Actions-OpenWrt/raw/main/0001-2305-Add-ZTE-E8820S-with-MTK-SDK-partition-support.patch
-patch -p1 -N < 0001-2305-Add-ZTE-E8820S-with-MTK-SDK-partition-support.patch
-rm  0001-2305-Add-ZTE-E8820S-with-MTK-SDK-partition-support.patch
+#wget https://gitee.com/jin3014/Actions-OpenWrt/raw/main/0001-2305-Add-ZTE-E8820S-with-MTK-SDK-partition-support.patch
+#patch -p1 -N < 0001-2305-Add-ZTE-E8820S-with-MTK-SDK-partition-support.patch
+#rm  0001-2305-Add-ZTE-E8820S-with-MTK-SDK-partition-support.patch
 
